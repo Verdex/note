038b29953bf16feaed8072ca862ecad788787707
David L. Inman <davidlevere@gmail.com>
2017-01-06 16:25:39 -0500

    went back on the else if thing; keywords should be done

--- Lex/Token.lua
+++ Lex/Token.lua
@@ -47,8 +47,8 @@ tokenType =
     ["else"] = "else";
     ["elseif"] = "elseif"; 
     ["if"] = "if";
-    while = "while";
-    break = "break";
+    ["while"] = "while";
+    ["break"] = "break";
     continue = "continue";
     ["in"] = "in";
     var = "var";
@@ -57,6 +57,8 @@ tokenType =
     using = "using";
     module = "module";
     foreach = "foreach"; 
+    ["true"] = "true";
+    ["false"] = "false";
 }
 
 tokenMaps = 
@@ -73,6 +75,144 @@ tokenMaps =
         end
     },
 
+    -- keywords
+    {
+        pattern = "true";
+        trans = function () return { type = tokenType["true"] } end 
+    },
 
+    {
+        pattern = "false";
+        trans = function () return { type = tokenType["false"] } end 
+    },
 
+    {
+        pattern = "do";
+        trans = function () return { type = tokenType["do"] } end 
+    },
+
+    {
+        pattern = "unit";
+        trans = function () return { type = tokenType["unit"] } end 
+    },
+
+    {
+        pattern = "enum";
+        trans = function () return { type = tokenType["enum"] } end 
+    },
+
+    {
+        pattern = "or";
+        trans = function () return { type = tokenType["or"] } end 
+    },
+
+    {
+        pattern = "and";
+        trans = function () return { type = tokenType["and"] } end 
+    },
+
+    {
+        pattern = "xor";
+        trans = function () return { type = tokenType["xor"] } end 
+    },
+
+    {
+        pattern = "not";
+        trans = function () return { type = tokenType["not"] } end 
+    },
+
+    {
+        pattern = "struct";
+        trans = function () return { type = tokenType["struct"] } end 
+    },
+
+    {
+        pattern = "return";
+        trans = function () return { type = tokenType["return"] } end 
+    },
+
+    {
+        pattern = "yield(%s+)return";
+        trans = function () return { type = tokenType["yieldReturn"] } end 
+    },
+
+    {
+        pattern = "yield(%s+)break";
+        trans = function () return { type = tokenType["yieldBreak"] } end 
+    },
+
+    {
+        pattern = "func";
+        trans = function () return { type = tokenType["func"] } end 
+    },
+
+    {
+        pattern = "const";
+        trans = function () return { type = tokenType["const"] } end 
+    },
+
+    {
+        pattern = "else";
+        trans = function () return { type = tokenType["else"] } end 
+    },
+
+    {
+        pattern = "else(%s+)if";
+        trans = function () return { type = tokenType["elseif"] } end 
+    },
+
+    {
+        pattern = "if";
+        trans = function () return { type = tokenType["if"] } end 
+    },
+
+    {
+        pattern = "while";
+        trans = function () return { type = tokenType["while"] } end 
+    },
+
+    {
+        pattern = "break";
+        trans = function () return { type = tokenType["break"] } end 
+    },
+
+    {
+        pattern = "continue";
+        trans = function () return { type = tokenType["continue"] } end 
+    },
+
+    {
+        pattern = "in";
+        trans = function () return { type = tokenType["in"] } end 
+    },
+
+    {
+        pattern = "var";
+        trans = function () return { type = tokenType["var"] } end 
+    },
+
+    {
+        pattern = "pub";
+        trans = function () return { type = tokenType["pub"] } end 
+    },
+
+    {
+        pattern = "type";
+        trans = function () return { type = tokenType["type"] } end 
+    },
+
+    {
+        pattern = "using";
+        trans = function () return { type = tokenType["using"] } end 
+    },
+
+    {
+        pattern = "module";
+        trans = function () return { type = tokenType["module"] } end 
+    },
+
+    {
+        pattern = "foreach";
+        trans = function () return { type = tokenType["foreach"] } end 
+    },
 }
