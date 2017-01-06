d6dfd9941c22d002c6fa2656b29e72b425d92a7a
David L. Inman <davidlevere@gmail.com>
2017-01-05 21:01:47 -0500

    initial lexer

--- Lex/Lexer.lua
+++ Lex/Lexer.lua
@@ -1,5 +1,36 @@
 
+-- input : string, tokMap : { pattern : regex; trans : [string] -> tok }
+function lex( input, tokMaps )
 
-function lex( )
+    local start = 1
+
+    local t = {}
+
+    -- todo handle the case where no pattern works
+    while start <= #input do
+
+        local success = false
+        for _, map in ipairs( tokMaps ) do 
+            local pattern = "^" ..  map.pattern .. "()"
+            local x = { string.match( input, pattern, start ) }
+
+            if x[1] then 
+                t[#t+1] = map.trans( x )  
+                --coroutine.yield( map.trans( x ) )
+
+                start = x[#x]
+
+                success = true
+                break
+            end
+        end
+        if not success then
+            assert( false, "failure" )
+        end
+
+    end
+
+    return t
 
 end
+
