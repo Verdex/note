2fa94cc94f1c86198ceaae0fa069894815625af4
David L. Inman <davidlevere@gmail.com>
2017-01-14 16:38:30 -0500

    converting lex to use coroutines

--- Lex/Lexer.lua
+++ Lex/Lexer.lua
@@ -1,37 +1,44 @@
 
+require "Token"
+
 -- input : string, tokMap : { pattern : regex; trans : [string] -> tok }
 function lex( input, tokMaps )
+    
+    return coroutine.wrap( function ()
+        local start = 1
 
-    local start = 1
+        local t = {}
 
-    local t = {}
+        while start <= #input do
 
-    while start <= #input do
+            local success = false
+            for _, map in ipairs( tokMaps ) do 
+                local pattern = "^" ..  map.pattern .. "()"
+                local result = { string.match( input, pattern, start ) }
 
-        local success = false
-        for _, map in ipairs( tokMaps ) do 
-            local pattern = "^" ..  map.pattern .. "()"
-            local result = { string.match( input, pattern, start ) }
+                if result[1] then 
 
-            if result[1] then 
-                t[#t+1] = map.trans( result )  
-                -- TODO
-                --coroutine.yield( map.trans( result ) )
+                    local tok = map.trans( result ) 
 
-                start = result[#result]
+                    if tok.type ~= tokenType.ignore then
 
-                success = true
-                break
+                        coroutine.yield( tok )
+                     
+                    end
+
+                    start = result[#result]
+
+                    success = true
+                    break
+                end
             end
-        end
-        if not success then
-            -- TODO need to handle failure by reporting index number
-            assert( false, "failure: " .. string.sub( input,  start - 20, start + 20 ) .. "\n" .. string.sub( input, start, start ))
-        end
+            if not success then
+                -- TODO add some error reporting
+                error(  "failure: " .. string.sub( input,  start - 20, start + 20 ) .. "\n" .. string.sub( input, start, start ))
+            end
 
-    end
-
-    return t
+        end
+    end )
 
 end
-
+        
