7da59865c0371fd91b5a21eeb99e528cc5eb476c
David L. Inman <davidlevere@gmail.com>
2017-02-13 22:26:02 -0500

    fixed problems with importing do notation

--- DoNotation.lua
+++ DoNotation.lua
@@ -2,78 +2,165 @@
 require "Utils"
 require "ParserUtils"
 
-local function symbol( buffer, index )
-    return match( tokenType.symbol, function ( s ) return s.value end )( buffer, index )
+doN = {}
+
+doN.tokenType = 
+{
+    ignore = "ignore";
+    symbol = "symbol";
+    openParen = "openParen";
+    closeParen = "closeParen";
+    openCurly = "openCurly";
+    closeCurly = "closeCurly";
+    comma = "comma";
+    arrow = "arrow";
+    noArrow = "noArrow";
+    ["do"] = "do";
+    unit = "unit";
+}
+
+doN.tokenMaps = 
+{
+    { 
+        pattern = [[//.-%c]];
+        trans = function () return { type = doN.tokenType.ignore } end
+    },
+
+    { 
+        pattern = [[/%*.-%*/]];
+        trans = function () return { type = doN.tokenType.ignore } end
+    },
+
+    { 
+        pattern = "%s";
+        trans = function () return { type = doN.tokenType.ignore } end
+    },
+
+    {
+        pattern = "do";
+        trans = function () return { type = doN.tokenType["do"] } end 
+    },
+
+    {
+        pattern = "unit(.-);";
+        trans = function ( s ) return { type = doN.tokenType.unit; value = s[1] } end
+    },
+
+    {
+        pattern = ",";
+        trans = function () return { type = doN.tokenType.comma } end
+    },
+
+
+    {
+        pattern = "<%-(.-);";
+        trans = function ( s ) return { type = doN.tokenType.arrow; value = s[1] } end
+    },
+
+    {
+        pattern = "|%-(.-);";
+        trans = function ( s ) return { type = doN.tokenType.noArrow; value = s[1] } end
+    },
+
+    {
+        pattern = "{";
+        trans = function () return { type = doN.tokenType.openCurly } end
+    },
+
+    {
+        pattern = "}";
+        trans = function () return { type = doN.tokenType.closeCurly } end
+    },
+
+    {
+        pattern = "%(";
+        trans = function () return { type = doN.tokenType.openParen } end
+    },
+
+    {
+        pattern = "%)";
+        trans = function () return { type = doN.tokenType.closeParen } end
+    },
+
+    {
+        pattern = "([_%a'][_%w']*)";
+        trans = function ( s ) return { type = doN.tokenType.symbol; value = s[1] } end
+    },
+}
+
+
+function doN.symbol( buffer, index )
+    return match( doN.tokenType.symbol, function ( s ) return s.value end )( buffer, index )
 end
 
-local function parseUnit( buffer, index )
-    return match( tokenType.unit, function ( s ) return s.value end )( buffer, index )
+function doN.parseUnit( buffer, index )
+    return match( doN.tokenType.unit, function ( s ) return s.value end )( buffer, index )
 end
 
-local function parseArrow( buffer, index )
-    return bind( symbol,                                                      function ( name ) return 
-           bind( match( tokenType.arrow, function ( s ) return s.value end ), function ( v )    return 
+function doN.parseArrow( buffer, index )
+    return bind( doN.symbol,                                                      function ( name ) return 
+           bind( match( doN.tokenType.arrow, function ( s ) return s.value end ), function ( v )    return 
            unit( { type = "arrow"; value = v ; var = name } ) end ) end )( buffer, index )
 end
 
-local function parseNoArrow( buffer, index )
-    return match( tokenType.noArrow, function ( s ) return { type = "noArrow" ; value = s.value } end )( buffer, index )
+function doN.parseNoArrow( buffer, index )
+    return match( doN.tokenType.noArrow, function ( s ) return { type = "noArrow" ; value = s.value } end )( buffer, index )
 end
 
-local function statement( buffer, index )
-    return choice { parseArrow
-                  , parseNoArrow
+function doN.statement( buffer, index )
+    return choice { doN.parseArrow
+                  , doN.parseNoArrow
                   } ( buffer, index )
 end
 
-local function statementList( buffer, index )
-    return choice { statementListTail
+function doN.statementList( buffer, index )
+    return choice { doN.statementListTail
                   , map( nothing, function () return {} end )
                   } ( buffer, index )
 end
 
-local function statementListTail( buffer, index )
-    return bind( statement,     function ( s )    return
-           bind( statementList, function ( rest ) return
+function doN.statementListTail( buffer, index )
+    return bind( doN.statement,     function ( s )    return
+           bind( doN.statementList, function ( rest ) return
            unit( insert( rest, 1, s ) ) end ) end )( buffer, index )
 end
 
-local function parseDoNotation( buffer, index )
-   return bind( check( tokenType["do"] ),      function ()           return
-          bind( check( tokenType.openParen ),  function ()           return
-          bind( symbol,                        function ( bindName ) return
-          bind( check( tokenType.comma ),      function ()           return
-          bind( symbol,                        function ( unitName ) return
-          bind( check( tokenType.closeParen ), function ()           return
-          bind( check( tokenType.openCurly ),  function ()           return
-          bind( statementList,                 function ( list )     return
-          bind( parseUnit,                     function ( u )        return
-          bind( check( tokenType.closeCurly ), function ()           return
+function doN.parseDoNotation( buffer, index )
+   return bind( check( doN.tokenType["do"] ),      function ()           return
+          bind( check( doN.tokenType.openParen ),  function ()           return
+          bind( doN.symbol,                        function ( bindName ) return
+          bind( check( doN.tokenType.comma ),      function ()           return
+          bind( doN.symbol,                        function ( unitName ) return
+          bind( check( doN.tokenType.closeParen ), function ()           return
+          bind( check( doN.tokenType.openCurly ),  function ()           return
+          bind( doN.statementList,                 function ( list )     return
+          bind( doN.parseUnit,                     function ( u )        return
+          bind( check( doN.tokenType.closeCurly ), function ()           return
           unit( { list = list; unitValue = u; bind = bindName; unit = unitName } ) end ) end ) end ) end ) end ) end ) end ) end ) end ) end )( buffer, index )
 end
 
-local function nextItem( list, index )
+function doN.nextItem( list, index )
     index = index + 1
     local n = list.list[index]
     if not n then
-        return unitGen( list.unit, list.unitValue )
+        return doN.unitGen( list.unit, list.unitValue )
     elseif n.type == "noArrow" then
-        return bindNoValGen(  n, list, index )
+        return doN.bindNoValGen(  n, list, index )
     elseif n.type == "arrow" then
-        return bindGen(  n, list, index )
+        return doN.bindGen(  n, list, index )
     end
 end
 
-local function unitGen( unitName, unitValue )
+function doN.unitGen( unitName, unitValue )
     return string.format( "%s ( %s )", unitName, unitValue )
 end
 
-local function bindNoValGen( bNode, list, index )
-    return string.format( "%s( %s, function () return\n%s end )", list.bind, bNode.value, nextItem( list, index ) )
+function doN.bindNoValGen( bNode, list, index )
+    return string.format( "%s( %s, function () return\n%s end )", list.bind, bNode.value, doN.nextItem( list, index ) )
 end
 
-local function bindGen( bNode, list, index)
-    return string.format( "%s( %s, function ( %s ) return\n%s end )", list.bind, bNode.value, bNode.var, nextItem( list, index ) )
+function doN.bindGen( bNode, list, index)
+    return string.format( "%s( %s, function ( %s ) return\n%s end )", list.bind, bNode.value, bNode.var, doN.nextItem( list, index ) )
 end
 
 
@@ -81,18 +168,18 @@ end
 -- unless you want to do weird caching/env stuff loading at point
 -- of usage is probably going to go better
 function doNotation( str )
-    local tokens = lex( str, tokenMaps )
-    local pass, buffer, index, value = parseDoNotation( tokens, 1 )
+    local tokens = doN.lex( str, doN.tokenMaps )
+    local pass, buffer, index, value = doN.parseDoNotation( tokens, 1 )
     if not pass then
         error "an error occurred while parsing the do notation"
     end
    
-    return string.format( "return function( buffer, index )\n    return %s ( buffer, index )\nend", nextItem( value, 0 ) )
+    return string.format( "return function( buffer, index )\n    return %s ( buffer, index )\nend", doN.nextItem( value, 0 ) )
 end
 
 
 -- input : string, tokMap : { pattern : regex; trans : [string] -> tok }
-local function lex( input, tokMaps )
+function doN.lex( input, tokMaps )
     
     local start = 1
 
@@ -109,7 +196,7 @@ local function lex( input, tokMaps )
 
                 local tok = map.trans( result ) 
 
-                if tok.type ~= tokenType.ignore then
+                if tok.type ~= doN.tokenType.ignore then
 
                     tokens[#tokens + 1] = tok
                  
@@ -132,89 +219,4 @@ local function lex( input, tokMaps )
 
 end
         
-
-local tokenType = 
-{
-    ignore = "ignore";
-    symbol = "symbol";
-    openParen = "openParen";
-    closeParen = "closeParen";
-    openCurly = "openCurly";
-    closeCurly = "closeCurly";
-    comma = "comma";
-    arrow = "arrow";
-    noArrow = "noArrow";
-    ["do"] = "do";
-    unit = "unit";
-}
-
-local tokenMaps = 
-{
-    { 
-        pattern = [[//.-%c]];
-        trans = function () return { type = tokenType.ignore } end
-    },
-
-    { 
-        pattern = [[/%*.-%*/]];
-        trans = function () return { type = tokenType.ignore } end
-    },
-
-    { 
-        pattern = "%s";
-        trans = function () return { type = tokenType.ignore } end
-    },
-
-    {
-        pattern = "do";
-        trans = function () return { type = tokenType["do"] } end 
-    },
-
-    {
-        pattern = "unit(.-);";
-        trans = function ( s ) return { type = tokenType.unit; value = s[1] } end
-    },
-
-    {
-        pattern = ",";
-        trans = function () return { type = tokenType.comma } end
-    },
-
-
-    {
-        pattern = "<%-(.-);";
-        trans = function ( s ) return { type = tokenType.arrow; value = s[1] } end
-    },
-
-    {
-        pattern = "|%-(.-);";
-        trans = function ( s ) return { type = tokenType.noArrow; value = s[1] } end
-    },
-
-    {
-        pattern = "{";
-        trans = function () return { type = tokenType.openCurly } end
-    },
-
-    {
-        pattern = "}";
-        trans = function () return { type = tokenType.closeCurly } end
-    },
-
-    {
-        pattern = "%(";
-        trans = function () return { type = tokenType.openParen } end
-    },
-
-    {
-        pattern = "%)";
-        trans = function () return { type = tokenType.closeParen } end
-    },
-
-    {
-        pattern = "([_%a'][_%w']*)";
-        trans = function ( s ) return { type = tokenType.symbol; value = s[1] } end
-    },
-}
-
 
